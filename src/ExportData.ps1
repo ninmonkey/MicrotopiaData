452db4ef28ffafed456648e0277eaa020e6e42c5
Jake Bolton <ninmonkeys@gmail.com>
2025-03-08 11:59:26 -0600

    feat: Started script to export

---
+++ src/ExportData.ps1
@@ -0,0 +1,41 @@
+﻿#Requires -Version 7
+if(-not (Import-Module 'ImportExcel' -PassThru -ea 'ignore')) {
+    Install-Module 'ImportExcel' -Scope CurrentUser -Confirm
+}
+$Paths = [ordered]@{
+    AppRoot = ($AppRoot = Get-Item $PSScriptRoot)
+    ExportRoot = Join-Path $AppRoot '../export'
+}
+
+$Paths.ExportRoot_CurrentVersion = Join-Path $Paths.ExportRoot 'v1.0.6a'
+
+$Paths.Xlsx_Prefabs   = Join-Path $Paths.ExportRoot_CurrentVersion 'prefabs.xlsx'
+$Paths.Xlsx_ChangeLog = Join-Path $Paths.ExportRoot_CurrentVersion 'changelog.xlsx'
+
+<#
+Main entry point.
+Todo: refactor
+#>
+
+. ( Get-Item -ea 'stop' (Join-Path '.' './MdUtils.ps1'))
+
+$pkg = Open-ExcelPackage -Path $Paths.xlsx_Prefabs
+# $pkg.Workbook.Worksheets | %{ $_.Name }
+$book = $pkg.Workbook
+# $sheet = $pkg.workbook.Worksheets
+hr
+md.Workbook.ListItems $Book
+Close-ExcelPackage -ExcelPackage $pkg
+
+$pkg = Open-ExcelPackage -Path $Paths.Xlsx_ChangeLog
+$book = $pkg.Workbook
+# $sheet = $pkg.workbook.Worksheets
+# $pkg.Workbook.Worksheets | %{ $_.Name }
+hr
+md.Workbook.ListItems $Book
+Close-ExcelPackage -ExcelPackage $pkg
+
+hr;
+
+
+$Paths | ft -AutoSize
---
+++ src/MdUtils.ps1
@@ -0,0 +1,37 @@
+function md.Workbook.ListItems {
+    <#
+    .SYNOPSIS
+        Takes a workbook, echo the list of tables defined
+    #>
+    param(
+        $InputObject
+    )
+    if( $book -isnot [OfficeOpenXml.ExcelWorkbook] ) {
+        "Unexpected value type: $( $InputObject.GetType() )" | Write-Warning
+    }
+    [OfficeOpenXml.ExcelWorkbook] $book = $InputObject
+    $joinStringSplat = @{
+        Separator    = ', '
+        Property     = 'Name'
+        SingleQuote  = $True
+        OutputPrefix = "WorkBook has Sheets: "
+    }
+
+    $book.Worksheets
+        | Join-String @joinStringSplat
+
+    $Book.Worksheets | % {
+        [OfficeOpenXml.ExcelWorksheet] $curSheet = $_
+
+        $joinStringSplat = @{
+            Separator    = ', '
+            Property     = 'Name'
+            SingleQuote  = $True
+            OutputPrefix = "ws $( $curSheet.Name ) has Tables: "
+        }
+
+        $curSheet.Tables
+            | Join-String @joinStringSplat
+    }
+
+}
