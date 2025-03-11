10cb6af45800d6fd2886e1394759246379438f90
ninmonkey <ninmonkeys@gmail.com>
2025-03-11 12:21:34 -0500

    new: exports workbook schemas, and consolidate functions

--- src/ExportData.ps1
+++ src/ExportData.ps1
@@ -1,93 +1,61 @@
 ﻿#Requires -Version 7
-if(-not (Import-Module 'ImportExcel' -PassThru -ea 'ignore')) {
+if(-not (Import-Module 'ImportExcel' -PassThru -ea 'silentlycontinue')) {
     Install-Module 'ImportExcel' -Scope CurrentUser -Confirm
 }
 $Paths = [ordered]@{
     AppRoot = ($AppRoot = Get-Item $PSScriptRoot)
     ExportRoot = Join-Path $AppRoot '../export'
 }
+if($true) { 
+    # todo: refactor as module 
+    $toImport = (Join-Path ($PSScriptRoot) './MdUtils.ps1')
+    "DotSrc: `"$toImport`"" | Out-Host
+    . ( Get-Item -ea 'stop' (Join-Path ($PSScriptRoot) './MdUtils.ps1'))
+}
+
 if( -not $function:Hr ) {    
     function Hr { "`n`n######`n`n" }
 }
 $Paths.ExportRoot_CurrentVersion = Join-Path $Paths.ExportRoot 'v1.0.6a'
 $Paths.ExportRoot_CurrentVersion = Join-Path $Paths.ExportRoot 'v1.0.7a'
 
+md.EnsureSubdirsExist -Path $paths.ExportRoot_CurrentVersion -verbose 
+
+$Paths.Xlsx_Biome = Join-Path $Paths.ExportRoot_CurrentVersion 'biome.xlsx'
+$Paths.Raw_Biome  = md.GetRawPath $Paths.Xlsx_Biome
+
 $Paths.Xlsx_Prefabs   = Join-Path $Paths.ExportRoot_CurrentVersion 'prefabs.xlsx'
+$Paths.Raw_Prefabs   = md.GetRawPath $Paths.Xlsx_Prefabs
+
 $Paths.Xlsx_ChangeLog = Join-Path $Paths.ExportRoot_CurrentVersion 'changelog.xlsx'
 $Paths.Md_ChangeLog   = Join-Path $Paths.ExportRoot_CurrentVersion 'changelog.md'
 $Paths.Csv_ChangeLog  = Join-Path $Paths.ExportRoot_CurrentVersion 'csv/changelog.csv'
 $Paths.json_ChangeLog = Join-Path $Paths.ExportRoot_CurrentVersion 'json/changelog.json'
+$Paths.json_WorkbookSchema = Join-Path $Paths.ExportRoot_CurrentVersion 'json/workbook-schema.json'
 
 # $Paths.Game = [ordered]@{
 #     'ProgramData_Root' = Join-Path 'C:\Program Files (x86)\Steam\steamapps\common\Microtopia' 'Microtopia_Data'
 # }
+
+
+
+
 <#
-Main entry point.
-Todo: refactor
+    Main entry point.
+    Todo: refactor
 #>
-function md.GetRawPath {
-    <#
-    .Synopsis 
-        Path or object, modifies path: 'foo.xlsx' => 'foo-raw.xlsx'
-    #>
-    [CmdletBinding()]
-    param( 
-        [object] $Path )
+# hr
 
-    $File = Get-Item -ea 'ignore' $Path
-    if( -not $File ) {
-        $File = [System.IO.FileInfo]::new( $Path ) 
-    }
-    if( -not $File) {
-        throw "Unhandled path type: $( $Path )"
-    }
-    $rawPath = Join-Path $File.DirectoryName "$( $File.BaseName )-raw.xlsx"
-    $rawPath
-}
+'export excel XLSX' | out-host ; #  Write-Host -fg 'orange'
+$pkg = Open-ExcelPackage -Path $Paths.Raw_Biome
+$book = $pkg.Workbook
+md.Workbook.ListItems $Book
+$sheets = $pkg.workbook.Worksheets
 
-function md.Export.Changelog {
-    <#
-    .SYNOPSIS
-        Exports changelog as '.csv', '.json', and '.md'
-    #>
-    [CmdletBinding()]
-    param()
+# detect column counts
+$curSheet = $pkg.Workbook.Worksheets['Biome Objects']
 
-    # $rawPath = $Paths.xlsx_Changelog 
-    # $rawFullJoin-Path $rawPath.DirectoryName "$( $_.baseName )-raw.xlsx"
-    $curOutput = $Paths.Xlsx_ChangeLog
-    $rawSrc = md.GetRawPath $curOutput
-
-    "md.Export.Changelog => Parse: $( $rawSrc ), Output: $( $curOutput )" | Write-Host -fg 'gray60' -bg 'gray30'
-
-    $imXl = Import-Excel -path $rawSrc -WorksheetName 'Changelog' -ImportColumns 1, 3 -HeaderName 'Version', 'English'
-    
-    # using BOM for best results when using Excel csv
-    $imXL
-        | ConvertTo-Csv
-        | Set-Content -Path ($Paths.Csv_ChangeLog) -Encoding utf8BOM
-
-    @( foreach($x in $imXl) {
-        '| {0} | {1} |' -f @(
-            $x.Version
-            $x.English
-        )
-    }) | Set-Content -Path $Paths.Md_ChangeLog -Encoding utf8
-
-    $imxl
-        | Select-Object -Prop Code, English
-        | ConvertTo-Json
-        | Set-Content -Path $Paths.json_ChangeLog
-
-    # $imXL | Join-String -p { $_.Version, $_.English } -sep "`n"
-
-
-    # @( foreach($record in $imXl) { $record.'Code', $record.'English' -join ' ' } ) | Join-String -sep "`n"
-}
-
-'done'
-. ( Get-Item -ea 'stop' (Join-Path ($PSScriptRoot) './MdUtils.ps1'))
-hr
+Close-ExcelPackage -ExcelPackage $pkg -NoSave
 
 
 return
--- src/MdUtils.ps1
+++ src/MdUtils.ps1
@@ -36,3 +36,139 @@ function md.Workbook.ListItems {
     }
 
 }
+
+function md.GetRawPath {
+    <#
+    .Synopsis 
+        Path or object, modifies path: 'foo.xlsx' => 'foo-raw.xlsx'
+    #>
+    [CmdletBinding()]
+    param( 
+        [object] $Path )
+
+    $File = Get-Item -ea 'ignore' $Path
+    if( -not $File ) {
+        $File = [System.IO.FileInfo]::new( $Path ) 
+    }
+    if( -not $File) {
+        throw "Unhandled path type: $( $Path )"
+    }
+    $rawPath = Join-Path $File.DirectoryName "$( $File.BaseName )-raw.xlsx"
+    $rawPath
+}
+
+function md.Export.Changelog {
+    <#
+    .SYNOPSIS
+        Exports changelog as '.csv', '.json', and '.md'
+    #>
+    [CmdletBinding()]
+    param()
+
+    # $rawPath = $Paths.xlsx_Changelog 
+    # $rawFullJoin-Path $rawPath.DirectoryName "$( $_.baseName )-raw.xlsx"
+    $curOutput = $Paths.Xlsx_ChangeLog
+    $rawSrc = md.GetRawPath $curOutput
+
+    "md.Export.Changelog => Parse: $( $rawSrc ), Output: $( $curOutput )" | Write-Host -fg 'gray60' -bg 'gray30'
+
+    $imXl = Import-Excel -path $rawSrc -WorksheetName 'Changelog' -ImportColumns 1, 3 -HeaderName 'Version', 'English'
+    
+    # using BOM for best results when using Excel csv
+    $imXL
+        | ConvertTo-Csv
+        | Set-Content -Path ($Paths.Csv_ChangeLog) -Encoding utf8BOM
+
+    @( foreach($x in $imXl) {
+        '| {0} | {1} |' -f @(
+            $x.Version
+            $x.English
+        )
+    }) | Set-Content -Path $Paths.Md_ChangeLog -Encoding utf8
+
+    $imxl
+        | Select-Object -Prop Code, English
+        | ConvertTo-Json
+        | Set-Content -Path $Paths.json_ChangeLog
+
+    # $imXL | Join-String -p { $_.Version, $_.English } -sep "`n"
+
+
+    # @( foreach($record in $imXl) { $record.'Code', $record.'English' -join ' ' } ) | Join-String -sep "`n"
+}
+
+function md.Workbook.Schema {
+    <#
+    .SYNOPSIS
+        filter files to *.xlsx, returns 'Get-ExcelFileSchema' as objects
+    #>
+    [CmdletBinding()]
+    param(
+        # Paths
+        [object[]] $Path,
+        [switch]$All
+    )
+
+    if($All) {
+        $sources = 
+            $Paths.GetEnumerator() 
+            | ?{ $_.Value -match '.*xlsx$' }
+    } else { 
+        $Sources = @( $Path ) 
+    }
+    
+    # emit
+    $Sources 
+        | ?{ Test-Path $_ }
+        | %{ 
+            Get-ExcelFileSchema -Path $_ 
+                | ConvertFrom-Json 
+        } 
+}
+
+function md.Export.WorkbookSchema {
+    <#
+    .synopsis
+        a quick summary of all worksheets, in all files as json. 
+    #>
+    [CmdletBinding()]
+    param(
+        [object[]] $Path,
+        $Destination
+    )
+    $Source = @( 
+        if( $Path ) { $Path }
+        else { $Paths.Values }
+    )
+    if( -not $Destination ) {
+        $Destination = $Paths.json_WorkbookSchema
+    }
+    $found = md.Workbook.Schema -Path $Source # $Paths.Values
+    $found 
+        | ConvertTo-Json -Depth 9 
+        | Set-Content -Path $Destination
+}
+
+function md.EnsureSubdirsExist {
+    <#
+    .SYNOPSIS
+        build any missing folders
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        # Root path of version directory
+        $Path,
+
+        [Parameter(Mandatory)]
+        [string[]] $RequiredNames = @('json', 'csv')
+    )
+        # mkdir (join-path $Paths.ExportRoot_CurrentVersion -ChildPath 'json') -ea 'ignore'
+
+    $versionRoot = Get-Item -ea 'stop' $Path
+    foreach( $name in $RequiredNames ) {
+        $newPath = Join-Path $versionRoot $name
+        $newPath | Join-String -f 'create: "{0}"' -sep ', ' | Write-Verbose
+        mkdir -ea 'ignore' $newPath 
+    }
+}
