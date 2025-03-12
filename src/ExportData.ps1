f929c772209850ce8cd8ad3d4dc1812504ca8950
Jake Bolton <ninmonkeys@gmail.com>
2025-03-11 22:51:46 -0500

    new output: `biome_objects-expanded.json`

--- src/ExportData.ps1
+++ src/ExportData.ps1
@@ -7,6 +7,8 @@ $Paths = [ordered]@{
     ExportRoot = Join-Path $AppRoot '../export'
 }
 if($true) {
+    Import-Module (Join-Path $PSScriptRoot 'Grouping.psm1') -ea 'stop'
+
     # todo: refactor as module
     $toImport = (Join-Path ($PSScriptRoot) './MdUtils.ps1')
     "DotSrc: `"$toImport`"" | Out-Host
@@ -32,6 +34,7 @@ $Paths.Md_ChangeLog   = Join-Path $Paths.ExportRoot_CurrentVersion 'changelog.md
 $Paths.Csv_ChangeLog  = Join-Path $Paths.ExportRoot_CurrentVersion 'csv/changelog.csv'
 $Paths.json_ChangeLog = Join-Path $Paths.ExportRoot_CurrentVersion 'json/changelog.json'
 $Paths.json_Biome_Objects = Join-Path $Paths.ExportRoot_CurrentVersion 'json/biome-objects.json'
+$Paths.json_Biome_Objects_Expanded = Join-Path $Paths.ExportRoot_CurrentVersion 'json/biome-objects-expanded.json'
 $Paths.json_WorkbookSchema = Join-Path $Paths.ExportRoot_CurrentVersion 'json/workbook-schema.json'
 
 # $Paths.Game = [ordered]@{
@@ -57,6 +60,8 @@ $sheets = $pkg.workbook.Worksheets
 
 # detect column counts
 $curSheet = $pkg.Workbook.Worksheets['Biome Objects']
+
+remove-item $Paths.Xlsx_Biome -ea 'Ignore'
 
 $importExcel_Splat = @{
     ExcelPackage  = $pkg
@@ -93,6 +98,10 @@ Export-Excel @exportExcel_Splat
 
 
 # json specific transforms
+$sort_splat = @{
+    Property = 'title', 'code', 'exchange_types'
+}
+
 $forJson = @(
     $Rows | %{
         $record = $_
@@ -105,15 +114,53 @@ $forJson = @(
         $record.'exchange_types'      = md.Parse.ItemsFromList $record.'exchange_types'
         $record.'unclickable'         = md.Parse.Checkbox $record.'unclickable'
         $record.'trails_pass_through' = md.Parse.Checkbox $record.'trails_pass_through'
-        $record
     }
-)
+) | Sort-Object @sortObjectSplat
 
 $forJson
     | ConvertTo-Json -depth 9
     | Set-Content -path $Paths.Json_Biome_Objects # -Confirm
 
 $Paths.Json_Biome_Objects | Join-String -f 'wrote: "{0}"' | write-host -fg 'gray50'
+
+
+# also emit expanded records
+$forJson = @(
+    $Rows | %{
+        $record = $_
+        $expandProp = 'exchange_types'
+        if( $record.$expandProp.count -gt 0) {
+            $record.$expandProp | %{
+                $curType            = $_
+                $newObj             = $record | Select-Object -Prop *
+                $newObj.$expandProp = $curType
+                $newObj
+            }
+        } else {
+            $record
+        }
+    }
+)| Sort-Object @sortObjectSplat
+
+
+$forJson
+    | ConvertTo-Json -depth 9
+    | Set-Content -path $Paths.json_Biome_Objects_Expanded # -Confirm
+
+$Paths.json_Biome_Objects_Expanded | Join-String -f 'wrote: "{0}"' | write-host -fg 'gray50'
+
+$exportExcel_Splat = @{
+    InputObject   = @( $forJson )
+    Path          = $Paths.Xlsx_Biome
+    Show          = $false
+    WorksheetName = 'Biome_Objects_Expanded'
+    TableName     = 'Biome_Objects_Expanded_Data'
+    TableStyle    = 'Light5'
+    Title         = 'From Json'
+    AutoSize      = $True
+}
+
+Export-Excel @exportExcel_Splat
 
 Close-ExcelPackage -ExcelPackage $pkg -NoSave
 
